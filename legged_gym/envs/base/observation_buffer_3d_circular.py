59d777d244cb44452dd2df7143970703dd07a69d
xuxin <747302550@qq.com>
2025-06-23 11:13:10 +0800

    add a test script for obs buffer

--- legged_gym/envs/base/observation_buffer_3d_circular.py
+++ legged_gym/envs/base/observation_buffer_3d_circular.py
@@ -41,3 +41,98 @@ class ObservationBuffer3D_circular:
         selected = selected.permute(0, 2, 1)
         # reshape to (num_envs, num_obs * len(obs_ids))
         return selected.reshape(self.num_envs, -1)
+
+def test2():
+    # speed test across three implementations
+    import time
+    num_envs = 4096
+    num_obs = 12
+    include_history_steps = 40
+    device = torch.device("cuda")
+    large_buffer1 = ObservationBuffer(num_envs, num_obs, include_history_steps, device)
+    large_buffer2 = ObservationBuffer3D(num_envs, num_obs, include_history_steps, device)
+    large_buffer3 = ObservationBuffer3D_circular(num_envs, num_obs, include_history_steps, device)
+
+    test_iter = 10000
+    obs = torch.rand(num_envs, num_obs, test_iter, device=device)
+
+    large_buffer1.insert(obs[...,0])
+    start = time.time()
+    for it in range(test_iter):
+        large_buffer1.insert(obs[...,it])
+    end = time.time()
+    print(f"{large_buffer1.__class__} insert {test_iter} using: {end-start:.4f} seconds")
+
+    large_buffer2.insert(obs[...,0])
+    start = time.time()
+    for it in range(test_iter):
+        large_buffer2.insert(obs[...,it])
+    end = time.time()
+    print(f"{large_buffer2.__class__} insert {test_iter} using: {end-start:.4f} seconds")
+
+    large_buffer3.insert(obs[...,0])
+    start = time.time()
+    for it in range(test_iter):
+        large_buffer3.insert(obs[...,it])
+    end = time.time()
+    print(f"{large_buffer3.__class__} insert {test_iter} using: {end-start:.4f} seconds")
+
+
+    r1 = large_buffer1.get_obs_vec(np.arange(include_history_steps))
+    r2 = large_buffer2.get_obs_vec(np.arange(include_history_steps))
+    r3 = large_buffer3.get_obs_vec(np.arange(include_history_steps))
+    diff2 = r2 - r1
+    diff3 = r3 - r1
+    if (diff2!=0).sum() >0:
+        print(it, (diff2!=0).sum())
+        print((diff2!=0).nonzero())
+        return
+    if (diff3!=0).sum() >0:
+        print(it, (diff3!=0).sum())
+        print((diff3!=0).nonzero())
+        return
+    print(diff2.mean().item(),diff3.mean().item())
+
+def test3():
+    # larget batch error test
+    num_envs = 400
+    num_obs = 12
+    include_history_steps = 40
+    device = torch.device("cuda")
+    large_buffer1 = ObservationBuffer(num_envs, num_obs, include_history_steps, device)
+    large_buffer2 = ObservationBuffer3D(num_envs, num_obs, include_history_steps, device)
+    large_buffer3 = ObservationBuffer3D_circular(num_envs, num_obs, include_history_steps, device)
+
+    test_iter = 10000
+    obs = torch.rand(num_envs, num_obs, test_iter, device=device)
+    obs_r = torch.zeros(num_envs, num_obs, device=device)
+
+    for it in range(test_iter):
+        large_buffer1.insert(obs[...,it])
+        large_buffer2.insert(obs[...,it])
+        large_buffer3.insert(obs[...,it])
+        r1 = large_buffer1.get_obs_vec(np.arange(include_history_steps))
+        r2 = large_buffer2.get_obs_vec(np.arange(include_history_steps))
+        r3 = large_buffer3.get_obs_vec(np.arange(include_history_steps))
+        diff2 = r2 - r1
+        diff3 = r3 - r1
+
+        if it % 100 == 0:
+            large_buffer1.reset(torch.arange(num_envs,device=device),obs_r)
+            large_buffer2.reset(torch.arange(num_envs,device=device),obs_r)
+            large_buffer3.reset(torch.arange(num_envs,device=device),obs_r)
+        
+        if (diff2!=0).sum() >0:
+            print(it, (diff2!=0).sum())
+            print((diff2!=0).nonzero())
+            return
+
+        if (diff3!=0).sum() >0:
+            print(it, (diff3!=0).sum())
+            print((diff3!=0).nonzero())
+            return
+    print(f"larget batch error test: ok")
+
+if __name__ == "__main__":
+    test2()
+    test3()
